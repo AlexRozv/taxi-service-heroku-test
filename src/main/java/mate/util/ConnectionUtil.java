46eada3c3e9cab20a117b086e6b42e77d1814a91
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2021-10-20 22:53:03 +0300

    added remote mysql database

--- src/main/java/mate/util/ConnectionUtil.java
+++ src/main/java/mate/util/ConnectionUtil.java
@@ -6,10 +6,9 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/taxi?useUnicode=true&use"
-            + "JDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    private static final String USERNAME = "username";
-    private static final String PASSWORD = "password";
+    private static final String URL = "jdbc:mysql://remotemysql.com:3306/7HWPbH67gC";
+    private static final String USERNAME = "7HWPbH67gC";
+    private static final String PASSWORD = "QRUPUq7NNH";
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 
     static {
