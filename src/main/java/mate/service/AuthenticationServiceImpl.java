ee71857d072c37ba8fa1aa4e4de25b68f62125b5
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2021-10-20 10:22:05 +0300

    fixed imports lexicographical order in AuthenticationServiceImpl

--- src/main/java/mate/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/service/AuthenticationServiceImpl.java
@@ -1,12 +1,12 @@
 package mate.service;
 
 import java.util.Optional;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import mate.exception.AuthenticationException;
 import mate.lib.Inject;
 import mate.lib.Service;
 import mate.model.Driver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
